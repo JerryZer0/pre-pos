fa04b180510decd39ea95ba0a5499743d825bd1d
JerryZer0 <jerry.zhang@oocl.com>
2018-07-03 23:14:46 +0800

    [jerry]hard work

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,25 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var num = new Array();
+	for (var i = 0; i < collectionA.length;) {
+		var count = 0;
+		for (var j = i; j < collectionA.length; j++) {
+			if (collectionA[i] === collectionA[j]) {
+				count++;
+			}
+		}
+		num.push({
+			key: collectionA[i],
+			count: count
+		})
+		i+=count;
+	}
+	var b = objectB.value.toString();
+	for(var i=0;i<num.length;i++){
+		if(b.indexOf(num[i].key.toString())>-1){
+			num[i].count=num[i].count-(num[i].count-num[i].count%3)/3;
+		}
+	}
+	return num;
 }
