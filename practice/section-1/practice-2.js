62e6f97768ec32eac170789009f98102ad4ecdb7
Jerry <jerry.zhang@oocl.com>
2018-07-03 19:17:59 +0800

    [jerry]1 finished

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var result = new Array();
+  var b=collectionB.toString();
+  for(var i=0;i<collectionA.length;i++){
+    if(b.indexOf(collectionA[i].toString())>-1)
+      result.push(collectionA[i]);
+  }
+  return result;
 }
